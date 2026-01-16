c98131c8fd77439a0ddbedf38cb691e10041cd47
Noe Deleon <SOLOYOYJEHOVA@OUTLOOK.COM>
2026-01-15 22:31:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   const productos = [
     { nombre: "Fade bajo", precio: 180, imagen: "Cortes/fade-bajo.png" },
-    { nombre: "Fade medio", precio: 180, imagen: "Cortes/fade-medio.jpg" },
-    { nombre: "Fade alto", precio: 180, imagen: "Cortes/fade-alto.jpg" },
-    { nombre: "Skin Fade", precio: 180, imagen: "Cortes/skin-fade.jpg" },
-    { nombre: "Buzz cut", precio: 180, imagen: "Cortes/buzz-cut.jpg" },
-    { nombre: "Crew Cut", precio: 180, imagen: "Cortes/crew-cut.jpg" },
-    { nombre: "French cut", precio: 180, imagen: "Cortes/french-cut.jpg" },
-    { nombre: "Caesar Cut", precio: 180, imagen: "Cortes/caesar-cut.jpg" },
-    { nombre: "Pompadour", precio: 180, imagen: "Cortes/pompadour.jpg" },
-    { nombre: "Quiff", precio: 180, imagen: "Cortes/quiff.jpg" },
-    { nombre: "Peinado hacia atras", precio: 180, imagen: "Cortes/peinado-hacia-atras.jpg" },
-    { nombre: "Undercut", precio: 180, imagen: "Cortes/undercut.jpg" },
-    { nombre: "Peinado al lado", precio: 180, imagen: "Cortes/peinado-al-lado.jpg" }
+    { nombre: "Fade medio", precio: 180, imagen: "Cortes/fade-medio.png" },
+    { nombre: "Fade alto", precio: 180, imagen: "Cortes/fade-alto.png" },
+    { nombre: "Skin Fade", precio: 180, imagen: "Cortes/skin-fade.png" },
+    { nombre: "Buzz cut", precio: 180, imagen: "Cortes/buzz-cut.png" },
+    { nombre: "Crew Cut", precio: 180, imagen: "Cortes/crew-cut.png" },
+    { nombre: "French cut", precio: 180, imagen: "Cortes/french-cut.png" },
+    { nombre: "Caesar Cut", precio: 180, imagen: "Cortes/caesar-cut.png" },
+    { nombre: "Pompadour", precio: 180, imagen: "Cortes/pompadour.png" },
+    { nombre: "Quiff", precio: 180, imagen: "Cortes/quiff.png" },
+    { nombre: "Peinado hacia atras", precio: 180, imagen: "Cortes/peinado-hacia-atras.png" },
+    { nombre: "Undercut", precio: 180, imagen: "Cortes/undercut.png" },
+    { nombre: "Peinado al lado", precio: 180, imagen: "Cortes/peinado-al-lado.png" }
   ];
 
   // ... el resto de tu código sigue igual ...
